4548880d8215e8dc510b04e215c05977628fef13
Ad van der Veer <advanderveer@gmail.com>
2023-03-21 08:20:51 +0100

    added benchmark for expr building

--- ddb/path_test.go
+++ ddb/path_test.go
@@ -3,10 +3,11 @@ package ddb_test
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	messagev1 "github.com/crewlinker/protoc-gen-dynamodb/proto/example/message/v1"
 )
 
-func BenchmarkPathBuilding(b *testing.B) {
+func BenchmarkOurPathBuilding(b *testing.B) {
 	b.ReportAllocs()
 	k1 := messagev1.Kitchen{
 		ExtraKitchen: &messagev1.Kitchen{
@@ -19,5 +20,14 @@ func BenchmarkPathBuilding(b *testing.B) {
 		if p1 != ".16.16.19[0].1" {
 			b.Fatalf("not valid path: %s", p1)
 		}
+	}
+}
+
+var p1 expression.NameBuilder
+
+func BenchmarkSdkPathBuilding(b *testing.B) {
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		p1 = expression.Name("16").AppendName(expression.Name("16")).AppendName(expression.Name("16")).AppendName(expression.Name("[1]"))
 	}
 }
